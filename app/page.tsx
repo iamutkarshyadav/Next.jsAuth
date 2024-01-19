7eb8f9136fde8dd9688b9ed0aca50eb2f0a73de0
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-19 20:52:28 +0530

    test1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button 
-      size={"lg"} variant={"outline"}>
-      <Link href={"/lol"}>Click Me</Link>
-    </Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to to-blue-800">
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg text-center">
+          A simple authentication system built with Next.js
+        </p>
+      </div>
+   </main>
   )
 }
